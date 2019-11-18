c8b5269449fe19b9493dce7bdc4a5ef65d85406c
Michael Angerman <stormasm@gmail.com>
2019-11-17 21:42:22 -0500

    json fields with i32 up and running

--- hn0/src/hna.rs
+++ hn0/src/hna.rs
@@ -6,6 +6,8 @@ pub struct Hna {
     pub by: String,
     pub title: String,
     pub url: String,
+    pub id: i32,
+    pub score: i32,
 }
 
 /// Builder for the `Hn` object.
@@ -14,6 +16,8 @@ pub struct HnaBuilder {
     by: Option<String>,
     title: Option<String>,
     url: Option<String>,
+    id: Option<i32>,
+    score: Option<i32>,
 }
 
 impl HnaBuilder {
@@ -38,11 +42,23 @@ impl HnaBuilder {
         self
     }
 
+    pub fn id<S: Into<i32>>(mut self, id: S) -> Self {
+        self.id = Some(id.into());
+        self
+    }
+
+    pub fn score<S: Into<i32>>(mut self, score: S) -> Self {
+        self.score = Some(score.into());
+        self
+    }
+
     pub fn build(self) -> Result<Hna, String> {
         Ok(Hna {
             title: self.title.ok_or("title missing")?,
             by: self.by.ok_or("by missing")?,
             url: self.url.ok_or("url missing")?,
+            id: self.id.ok_or("id missing")?,
+            score: self.score.ok_or("score missing")?,
             ..Default::default()
         })
     }
