c44f8b797bd4b62480ff651752b458117c79f8e2
Michael Angerman <stormasm@gmail.com>
2019-12-21 08:52:07 -0500

    mw

--- map/examples/map.rs
+++ map/examples/map.rs
@@ -1,12 +1,9 @@
-
-fn main() {
-
 use serde_json::{Map, Value};
 
-let mut m = Map::new();
-m.insert("Lorem".to_string(), "ipsum".into());
-let x: Value = m.into();
-println!("{}",x);
-
-
+fn main() {
+    let mut m = Map::new();
+    m.insert("Lorem".to_string(), "ipsum".into());
+    m.insert("count".to_string(), 4.into());
+    let x: Value = m.into();
+    println!("{}", x);
 }
